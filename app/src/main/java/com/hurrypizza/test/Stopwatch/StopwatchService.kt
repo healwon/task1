3ea8240366f4d65a513c1393d5c4d9a00820b6fe
YewonChoi <woni9911@gmail.com>
2021-01-02 23:14:30 +0900

    notificationchannel

--- app/src/main/java/com/hurrypizza/test/Stopwatch/StopwatchService.kt
+++ app/src/main/java/com/hurrypizza/test/Stopwatch/StopwatchService.kt
@@ -1,14 +1,26 @@
 package com.hurrypizza.test.Stopwatch
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.media.app.NotificationCompat
+import com.hurrypizza.test.MainActivity
+import com.hurrypizza.test.R
 import java.util.*
 import kotlin.collections.ArrayList
+import androidx.core.app.NotificationManagerCompat as NotificationManagerCompat1
 
 class StopwatchService: Service() {
+    val CHANNEL_ID = "StopwatchForegroundServiceChannel"
+
     private var time = 0
     private var isNotZero = false
     private var isRunning = false
@@ -23,21 +35,27 @@ class StopwatchService: Service() {
         fun getService(): StopwatchService = this@StopwatchService
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        Log.d("service($id)","onBind()")
-        return myBinder
+    override fun onCreate() {
+        id = Random().nextInt(100)
+        Log.d("service($id)","onCreate()")
+        super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
         Log.d("service($id)","onStartCommand()")
+        createNotificationChannel()
         return START_REDELIVER_INTENT
     }
 
-    override fun onCreate() {
-        id = Random().nextInt(100)
-        Log.d("service($id)","onCreate()")
-        super.onCreate()
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.d("service($id)","onBind()")
+        return myBinder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.d("service($id)","onUnbind()")
+        return super.onUnbind(intent)
     }
 
     override fun onDestroy() {
@@ -45,9 +63,26 @@ class StopwatchService: Service() {
         super.onDestroy()
     }
 
-    override fun onUnbind(intent: Intent?): Boolean {
-        Log.d("service($id)","onUnbind()")
-        return super.onUnbind(intent)
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "name"
+            val descriptionText = "description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun startForegroundService() {
+        //val notification = androidx.core.app.NotificationCompat.Builder(this, CHANNEL_ID)
+        //startForeground(1, notification)
     }
 
     fun startAndPause() {
