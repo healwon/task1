01d2d74a941bd2b6fb028252ae820c2056be1f84
YewonChoi <woni9911@gmail.com>
2021-01-05 16:31:04 +0900

    unused file deleted/ tab1,3 코드 정리

--- app/src/main/java/com/hurrypizza/test/Stopwatch/StopwatchService.kt
+++ app/src/main/java/com/hurrypizza/test/Stopwatch/StopwatchService.kt
@@ -1,20 +1,16 @@
 package com.hurrypizza.test.Stopwatch
 
 import android.app.*
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.media.app.NotificationCompat
 import com.hurrypizza.test.MainActivity
 import com.hurrypizza.test.R
 import java.util.*
 import kotlin.collections.ArrayList
-import androidx.core.app.NotificationManagerCompat as NotificationManagerCompat1
 
 class StopwatchService: Service() {
     val CHANNEL_ID = "StopwatchForegroundServiceChannel"
@@ -27,7 +23,7 @@ class StopwatchService: Service() {
     private var notiTask: Timer? = null
     private var id: Int = 0
 
-    val records: ArrayList<Int> = ArrayList<Int>()
+    val records: ArrayList<Int> = ArrayList()
 
     private val myBinder = MyBinder()
 
@@ -37,30 +33,17 @@ class StopwatchService: Service() {
 
     override fun onCreate() {
         id = Random().nextInt(100)
-        Log.d("service($id)","onCreate()")
         createNotificationChannel()
         super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        Log.d("service($id)","onStartCommand()")
         return START_REDELIVER_INTENT
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        Log.d("service($id)","onBind()")
         return myBinder
-    }
-
-    override fun onUnbind(intent: Intent?): Boolean {
-        Log.d("service($id)","onUnbind()")
-        return super.onUnbind(intent)
-    }
-
-    override fun onDestroy() {
-        Log.d("service($id)","onDestroy()")
-        super.onDestroy()
     }
 
     private fun createNotificationChannel() {
@@ -94,12 +77,11 @@ class StopwatchService: Service() {
     }
 
     fun startForegroundService() {
-        Log.d("service($id)","startFS()")
         val notificationIntent = Intent(this, MainActivity::class.java)
         notificationIntent.putExtra("tabIndex", 2)
         val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        var notification = createNotification(pendingIntent)
+        val notification = createNotification(pendingIntent)
         startForeground(NOFIFYCATION_ID, notification)
         if (isRunning) {
             val notificationManager: NotificationManager =
@@ -112,7 +94,6 @@ class StopwatchService: Service() {
 
     fun stopForegroundService() {
         if (notiTask != null) {
-            Log.d("service($id)","stopFS()")
             notiTask?.cancel()
             notiTask = null
         }
